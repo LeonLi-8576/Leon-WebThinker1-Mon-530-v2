fb4ffac8042e37f3961dd9ac1fe77c3c9f7a8684
LeonLi-8576 <leonli8756@gmail.com>
2024-10-21 11:04:19 +0000

    21.10 Lesson: AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA

--- script/projects.js
+++ script/projects.js
@@ -1,1 +1,23 @@
-// 
+window.onload = () =>{
+    const Canvas = document.getElementById("CanvasBoard");
+    let Contacts = Canvas.getContext("2d");
+
+    // Contacts.beginPath();
+    // Contacts.moveTo(0,0);
+    // Contacts.lineTo(100,100);
+    // Contacts.lineTo(800,600);
+    // Contacts.stroke();
+
+    // Contacts.fillStyle = "rgba(96, 214, 204, 0.5)";
+    // Contacts.fillRect(10,10,100,100);
+    // Contacts.fillStyle = "rgba(179, 196, 27, 0.7)";
+    // Contacts.fillRect(80,80,100,100);
+
+    // Contacts.beginPath();
+    // Contacts.arc(120, 120, 100, 0, 2 * Math.PI);
+    // Contacts.lineWidth = 10;
+    // Contacts.strokeStyle = "rgb(0, 0, 0)";
+    // Contacts.fillStyle = "rgb(0, 0, 255)";
+    // Contacts.stroke();
+    // Contacts.fill();
+}
